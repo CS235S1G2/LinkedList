70d0b437567e6ca0c140ec75190c9bc63c174a66
Justin Chandler <justinchandler.17@gmail.com>
2017-05-27 00:00:29 -0600

    Update node.h

--- node.h
+++ node.h
@@ -156,7 +156,7 @@ void freeData(Node <T> * &pHead)
    for (Node <T> * p = pHead; p; p = p->pNext)
    {
       delete p;
-      cout << "deleting p\n";
+      std::cout << "deleting p\n";
    }
     pHead = NULL;
 }
